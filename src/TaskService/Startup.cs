ff8daead0feda1523a4552137d43004dd0cd3a7c
BelehovEgor <belehov.egor2001@yandex.ru>
2021-11-15 11:25:06 +0300

    update startup

--- src/TaskService/Startup.cs
+++ src/TaskService/Startup.cs
@@ -72,16 +72,44 @@ namespace LT.DigitalOffice.TaskService
 
     #region configure masstransit
 
+    private (string username, string password) GetRabbitMqCredentials()
+    {
+      static string GetString(string envVar, string formAppsettings, string generated, string fieldName)
+      {
+        string str = Environment.GetEnvironmentVariable(envVar);
+        if (string.IsNullOrEmpty(str))
+        {
+          str = formAppsettings ?? generated;
+
+          Log.Information(
+            formAppsettings == null
+              ? $"Default RabbitMq {fieldName} was used."
+              : $"RabbitMq {fieldName} from appsetings.json was used.");
+        }
+        else
+        {
+          Log.Information($"RabbitMq {fieldName} from environment was used.");
+        }
+
+        return str;
+      }
+
+      return (GetString("RabbitMqUsername", _rabbitMqConfig.Username, $"{_serviceInfoConfig.Name}_{_serviceInfoConfig.Id}", "Username"),
+        GetString("RabbitMqPassword", _rabbitMqConfig.Password, _serviceInfoConfig.Id, "Password"));
+    }
+
     private void ConfigureMassTransit(IServiceCollection services)
     {
+      (string username, string password) = GetRabbitMqCredentials();
+
       services.AddMassTransit(busConfigurator =>
       {
         busConfigurator.UsingRabbitMq((context, cfg) =>
         {
           cfg.Host(_rabbitMqConfig.Host, "/", host =>
           {
-            host.Username($"{_serviceInfoConfig.Name}_{_serviceInfoConfig.Id}");
-            host.Password(_serviceInfoConfig.Id);
+            host.Username(username);
+            host.Password(password);
           });
         });
 
